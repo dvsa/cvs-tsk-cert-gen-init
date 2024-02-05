abd0cb6cc8941e5f509215c0f3249ae2b4d3e7a0
Craig Broady <79261988+cb-cs@users.noreply.github.com>
2024-02-05 11:49:45 +0000

    CB2-9865: Failed IVA test triggers cert-gen (#62)
    * feat(cb2-9865): added test for coverage and bumped crit dependency
    
    * feat(cb2-9865): updated logic and tests
    
    * feat(cb2-9865): updated logic and added more tests
    
    * feat(cb2-9865): added parenthesis to group additional logic for IVA30s
    
    * feat(cb2-9865): formatting
    
    * feat(cb2-9865): removed obsolete async in tests
    
    * feat(cb2-9865): updated logic as per pr

--- tests/unit/utils.unitTest.ts
+++ tests/unit/utils.unitTest.ts
@@ -15,7 +15,7 @@ describe("utils", () => {
   });
 
   describe("when filtering dynamoDB streams events", () => {
-    it("should filter correctly cancelled tests", async () => {
+    it("should filter correctly cancelled tests", () => {
       expandedRecords[0].testStatus = "cancelled";
       const filteredRecords: any[] =
         Utils.filterCertificateGenerationRecords(expandedRecords);
@@ -23,7 +23,7 @@ describe("utils", () => {
       expect(filteredRecords.length).toBe(0);
     });
 
-    it("should filter correctly abandoned testTypes", async () => {
+    it("should filter correctly abandoned testTypes", () => {
       console.log(expandedRecords[0]);
       expandedRecords[0].testTypes.testResult = "abandoned";
       const filteredRecords: any[] =
@@ -32,19 +32,77 @@ describe("utils", () => {
       expect(filteredRecords.length).toBe(0);
     });
 
-    it("should not remove events which have to generate a certificate", async () => {
+    it("should not remove events which have to generate a certificate", () => {
       const filteredRecords: any[] =
         Utils.filterCertificateGenerationRecords(expandedRecords);
 
       expect(filteredRecords).toEqual(expandedRecords);
     });
 
-    it("should filter correctly events without a testTypeClassification ", async () => {
+    it("should filter correctly events without a testTypeClassification ", () => {
       delete expandedRecords[0].testTypes.testTypeClassification;
       const filteredRecords: any[] =
         Utils.filterCertificateGenerationRecords(expandedRecords);
 
       expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should filter correctly events that have IVA With Certificate, ivaDefects populated but not test result fail", () => {
+      expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+      expandedRecords[0].testTypes.ivaDefects = [{}];
+      expandedRecords[0].testTypes.testResult = "pass";
+      const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+      expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should filter correctly events that have IVA With Certificate, test result fail but no ivaDefects", () => {
+        expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should filter correctly events that have IVA With Certificate, test result fail but empty ivaDefects", () => {
+        expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+        expandedRecords[0].testTypes.testResult = "fail";
+        expandedRecords[0].testTypes.ivaDefects = [];
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should not remove events which have IVA With Certificate, ivaDefects and test result fail", () => {
+      expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+      expandedRecords[0].testTypes.testResult = "fail";
+      expandedRecords[0].testTypes.ivaDefects = [{}];
+      const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+      expect(filteredRecords).toEqual(expandedRecords);
+    });
+
+    it("should not remove events which have Annual With Certificate, no ivaDefects and test result fail", () => {
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords).toEqual(expandedRecords);
+    });
+
+    it("should not remove events which have Annual With Certificate, empty ivaDefects and test result fail", () => {
+        expandedRecords[0].testTypes.ivaDefects = [];
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords).toEqual(expandedRecords);
+    });
+
+    it("should not remove events which have Annual With Certificate, populated ivaDefects and test result fail", () => {
+        expandedRecords[0].testTypes.ivaDefects = [{}];
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords).toEqual(expandedRecords);
     });
   });
 });
