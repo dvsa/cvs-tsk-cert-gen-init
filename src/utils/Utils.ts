014c1ba6ad9535561cbe6fc1bd0e94ebf6e29f80
Craig Broady <79261988+cb-cs@users.noreply.github.com>
2024-03-01 13:29:58 +0000

    CB2-10687: Update Cert-Gen-Init to trigger for IVA30 (#66)
    * CB2-9865: Failed IVA test triggers cert-gen (#62)
    
    * feat(cb2-9865): added test for coverage and bumped crit dependency
    
    * feat(cb2-9865): updated logic and tests
    
    * feat(cb2-9865): updated logic and added more tests
    
    * feat(cb2-9865): added parenthesis to group additional logic for IVA30s
    
    * feat(cb2-9865): formatting
    
    * feat(cb2-9865): removed obsolete async in tests
    
    * feat(cb2-9865): updated logic as per pr
    
    * feat(cb2-10870): refactored any ivaDefects references to requiredStandards (#65)

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -21,15 +21,15 @@ export class Utils {
         );
       })
       .filter((record: any) => {
-        // Filter by testTypeClassification
-        if (record.testTypes && record.testTypes.testTypeClassification) {
-          return (
-            record.testTypes.testTypeClassification ===
-            "Annual With Certificate"
-          );
-        }
+        // Filter by testTypeClassification or testTypeClassification, testResult and requiredStandards present and populated
+        const testTypes = record.testTypes;
 
-        return false;
+        const isAnnualWithCertificate = testTypes?.testTypeClassification === "Annual With Certificate";
+        const isIvaWithCertificate = testTypes?.testTypeClassification === "IVA With Certificate";
+        const isTestResultFail = testTypes?.testResult === "fail";
+        const hasNonEmptyRequiredStandards = !!(testTypes?.requiredStandards?.length);
+
+        return isAnnualWithCertificate || (isIvaWithCertificate && isTestResultFail && hasNonEmptyRequiredStandards);
       });
   }
 }
--- tests/unit/utils.unitTest.ts
+++ tests/unit/utils.unitTest.ts
@@ -15,7 +15,7 @@ describe("utils", () => {
   });
 
   describe("when filtering dynamoDB streams events", () => {
-    it("should filter correctly cancelled tests", async () => {
+    it("should filter correctly cancelled tests", () => {
       expandedRecords[0].testStatus = "cancelled";
       const filteredRecords: any[] =
         Utils.filterCertificateGenerationRecords(expandedRecords);
@@ -23,7 +23,7 @@ describe("utils", () => {
       expect(filteredRecords.length).toBe(0);
     });
 
-    it("should filter correctly abandoned testTypes", async () => {
+    it("should filter correctly abandoned testTypes", () => {
       console.log(expandedRecords[0]);
       expandedRecords[0].testTypes.testResult = "abandoned";
       const filteredRecords: any[] =
@@ -32,19 +32,77 @@ describe("utils", () => {
       expect(filteredRecords.length).toBe(0);
     });
 
-    it("should not remove events which have to generate a certificate", async () => {
+    it("should not remove events which have to generate a certificate", () => {
       const filteredRecords: any[] =
         Utils.filterCertificateGenerationRecords(expandedRecords);
 
       expect(filteredRecords).toEqual(expandedRecords);
     });
 
-    it("should filter correctly events without a testTypeClassification ", async () => {
+    it("should filter correctly events without a testTypeClassification ", () => {
       delete expandedRecords[0].testTypes.testTypeClassification;
       const filteredRecords: any[] =
         Utils.filterCertificateGenerationRecords(expandedRecords);
 
       expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should filter correctly events that have IVA With Certificate, requiredStandards populated but not test result fail", () => {
+      expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+      expandedRecords[0].testTypes.requiredStandards = [{}];
+      expandedRecords[0].testTypes.testResult = "pass";
+      const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+      expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should filter correctly events that have IVA With Certificate, test result fail but no requiredStandards", () => {
+        expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should filter correctly events that have IVA With Certificate, test result fail but empty requiredStandards", () => {
+        expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+        expandedRecords[0].testTypes.testResult = "fail";
+        expandedRecords[0].testTypes.requiredStandards = [];
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords.length).toBe(0);
+    });
+
+    it("should not remove events which have IVA With Certificate, requiredStandards and test result fail", () => {
+      expandedRecords[0].testTypes.testTypeClassification = "IVA With Certificate";
+      expandedRecords[0].testTypes.testResult = "fail";
+      expandedRecords[0].testTypes.requiredStandards = [{}];
+      const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+      expect(filteredRecords).toEqual(expandedRecords);
+    });
+
+    it("should not remove events which have Annual With Certificate, no requiredStandards and test result fail", () => {
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords).toEqual(expandedRecords);
+    });
+
+    it("should not remove events which have Annual With Certificate, empty requiredStandards and test result fail", () => {
+        expandedRecords[0].testTypes.requiredStandards = [];
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords).toEqual(expandedRecords);
+    });
+
+    it("should not remove events which have Annual With Certificate, populated requiredStandards and test result fail", () => {
+        expandedRecords[0].testTypes.requiredStandards = [{}];
+        expandedRecords[0].testTypes.testResult = "fail";
+        const filteredRecords: any[] = Utils.filterCertificateGenerationRecords(expandedRecords);
+
+        expect(filteredRecords).toEqual(expandedRecords);
     });
   });
 });
